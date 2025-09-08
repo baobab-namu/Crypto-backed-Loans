18dd5edd6fa508fa4863659188f990f23fb68464
seongju.lee <seongju.lee@bdacs.co.kr>
2025-09-08 19:03:06 +0900

    feat: init project

---
+++ src/main/kotlin/playground/cryptobackedloans/CryptoBackedLoansApplication.kt
@@ -0,0 +1,11 @@
+package playground.cryptobackedloans
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CryptoBackedLoansApplication
+
+fun main(args: Array<String>) {
+    runApplication<CryptoBackedLoansApplication>(*args)
+}
